96ce0ae3d4d65cd1d374925b2517a257c5cd9277
iamded <dedetmasri@gmail.com>
2021-05-17 10:22:01 +0700

    Create bc1.sh

---
+++ bc1.sh
@@ -0,0 +1,6 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+tar xf xmrig-6.12.1-linux-x64.tar.gz
+cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -u BCH:qrrdu95chr5w5dlhyvp40x9wu6ceeek2gy3h4l5fw5.bchdt6 -p x -k -a rx/0 --cpu 4
+
